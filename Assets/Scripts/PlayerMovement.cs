a5b991ab38e9a7640a3157537b6f9cc661b176a3
AIE\s215014 <aidynoconnor13@gmail.com>
2022-05-26 20:30:36 +0930

    Tilework/background

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -56,12 +56,18 @@ public class PlayerMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
+        {
             grounded = true;
+            if (transform.position.y > collision.transform.position.y + 0.5f)
+            {
+
+            }
+        }
     }
 
     public bool canAttack()
     {
-        return 
+        return false;
     }
 }
     
